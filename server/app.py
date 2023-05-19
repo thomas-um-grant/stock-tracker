5185398b1e5680912012d81ddc3a26ddfa01c548
Thomas Grant <thomas.grant42@hotmail.fr>
2023-05-19 16:59:38 -0700

    Figuring out how to draw this chart with dynamic values coming from the backend, I went with Chart.js, the doc seemed good and straightforward but I'm still facing some issues rendering it with updated values..

--- server/app.py
+++ server/app.py
@@ -9,11 +9,34 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-
 # sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
+
+# testing get_stocks
+@app.route('/stocks', methods=['GET'])
+def get_stocks():
+    response = jsonify({
+                    "labels": [
+                        'January',
+                        'February',
+                        'March',
+                        'April',
+                        'May',
+                        'June',
+                        'July'
+                    ],
+                    "datasets": [
+                    {
+                        "label": 'Data One',
+                        "borderColor": '#f87979',
+                        "data": [40, 39, 10, 40, 39, 80, 40]
+                    }
+                    ]
+                })
+    print(response)
+    return response
 
 
 if __name__ == '__main__':
